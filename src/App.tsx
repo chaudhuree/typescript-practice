2b46dfa294d145b43aa10004e9b64c392382fc84
chaudhuree <chaudhuree00@gmail.com>
2022-09-25 02:18:50 +0600

    basic props passing

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import "./App.css";
 import Person from "./components/Person";
 
 function App() {
-
   return (
     <div className="App">
-     <Person name="chaudhuree" age={27}></Person>
+      <Person name="chaudhuree" age={27}></Person>
     </div>
   );
 }
