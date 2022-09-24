8e345c286c8b28df2828d5a039a5bd577ef4ba62
chaudhuree <chaudhuree00@gmail.com>
2022-09-25 05:06:53 +0600

    onchange event type

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from "react";
 import "./App.css";
 import Person from "./components/Person";
-import {PersonFc} from "./components/PersonFC";
+import { PersonFc } from "./components/PersonFC";
+import Input from "./components/Input";
 
 const App: FC = () => {
   return (
     <div className="App">
       <Person name="chaudhuree" age={27} />
       <PersonFc age={28} name="sOhan" />
+      <Input />
     </div>
   );
 };
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,7 +2,21 @@ import { FC, ChangeEvent, useState } from "react";
 
 const Input: FC = () => {
   const [country, setCountry] = useState<string | null>(null);
-  return <div></div>;
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCountry(event.target.value);
+  };
+  return (
+    <div>
+      <h1>Input</h1>
+
+      <input
+        type="text"
+        placeholder="input your name"
+        onChange={handleChange}
+      />
+      <h2>{country}</h2>
+    </div>
+  );
 };
 
 export default Input;
